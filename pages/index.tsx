dd8775c8725bd4a272dc45ef4fb7a58d4f57c605
itsspriyansh <priyanshkp01@gmail.com>
2023-05-19 17:22:08 +0530

    added protected route at home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,23 @@
-import { signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps (context : NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect : {
+        destination : "/Auth",
+        permanent : false
+      }
+    }
+  }
+
+  return {
+    props : {}
+  }
+}
+
 
 export default function Home() {
   return (
