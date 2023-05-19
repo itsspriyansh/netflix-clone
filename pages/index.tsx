307ac5213847ea212b76108723c4fb70a3111da5
itsspriyansh <priyanshkp01@gmail.com>
2023-05-19 17:13:15 +0530

    serverAuth swr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import { signOut } from "next-auth/react";
+
 export default function Home() {
   return (
+    <>
     <div className=" text-green-500">Home Page</div>
+    <button
+      onClick={() => signOut()} 
+      className="bg-white px-5 py-2">Logout</button>
+    </>
   )
 }
